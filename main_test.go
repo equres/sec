35bf0d4f69c15859b6a7a9134e5f9ae88b3c77e2
Hazeman99 <hazem.xbox@gmail.com>
2021-08-19 08:59:39 +0800

    added http serving and downloading index

---
+++ main_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Equres LLC. All rights reserved.
+package main
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/equres/sec/util"
+)
+
+//go:embed migrations
+var MigrationsFS embed.FS
+
+// Serve file in HTTP and download to testdata directory
+func TestHTTPDownloadFile(t *testing.T) {
+	config, err := util.LoadConfig("./ci")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Path for files
+	filesPath := "cache/Archives/edgar/monthly"
+	testServer := httptest.NewServer(http.FileServer(http.Dir(filesPath)))
+
+	defer testServer.Close()
+
+	sec := util.NewSEC(testServer.URL)
+
+	err = sec.DownloadFile(fmt.Sprintf("%v/%v", sec.BaseURL, "xbrlrss-2021-04.xml"), config)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
