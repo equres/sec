5bdbdd2573805348f0064b39a38a111e195a159c
Hazeman99 <hazem.xbox@gmail.com>
2021-08-25 08:27:38 +0800

    fixed testing all file

--- scripts/test_all_years.sh
+++ scripts/test_all_years.sh
@@ -1,9 +1,9 @@
 for Year in {2006..2021}
 do
-    ../sec dd $Year
-    for Month in {1..12}:
+    ./sec dd $Year
+    for Month in {1..12}
     do
-        echo trying $Year $Month
+        echo trying $Year/$Month
         ./sec de $Year/$Month
         ./sec dest
     done
