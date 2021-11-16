1b22d9edea2a4b5a343b9f86e332432fc367b860
Hazeman99 <hazem.xbox@gmail.com>
2021-11-17 04:47:38 +0800

    Update sec_backup.sh

--- infra/root/sec_backup.sh
+++ infra/root/sec_backup.sh
@@ -1,6 +1,7 @@
 . .creds
 
 lftp -c "set ftp:list-options -a;
+set ssl:check-hostname no;
 open ftp://$login:$password@$FTP_server; 
-cd /mnt/sec/cache;
+lcd /mnt/sec/cache;
 mirror --reverse --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;"
