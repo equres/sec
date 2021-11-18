a4140e4995dfeaac5ca70f6db54cd5fac0ef5f02
Hazeman99 <hazem.xbox@gmail.com>
2021-11-19 01:54:14 +0800

    logging to syslog-ng

--- infra/root/backup_restore.sh
+++ infra/root/backup_restore.sh
@@ -4,4 +4,4 @@ lftp -c "set ftp:list-options -a;
 set ssl:check-hostname no;
 open ftp://$login:$password@$FTP_server; 
 lcd /mnt/sec/cache;
-mirror --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;"
+mirror --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;" | logger
