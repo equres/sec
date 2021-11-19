cf90f6f2c3215b04c84584c907d4f422535369bb
Hazeman99 <hazem.xbox@gmail.com>
2021-11-20 03:27:52 +0800

    updated backup files

--- infra/root/backup_restore.sh
+++ infra/root/backup_restore.sh
@@ -3,5 +3,5 @@
 lftp -c "set ftp:list-options -a;
 set ssl:check-hostname no;
 open ftp://$login:$password@$FTP_server; 
-lcd /mnt/sec/cache;
+lcd /home/sec/backup;
 mirror --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;" | logger
--- infra/root/backup_verify.sh
+++ infra/root/backup_verify.sh
@@ -1,7 +1,1 @@
-. .creds
-
-lftp -c "set ftp:list-options -a;
-set ssl:check-hostname no;
-open ftp://$login:$password@$FTP_server; 
-lcd /mnt/sec/cache;
-mirror --dry-run --verbose --allow-chown  --allow-suid --no-umask;" | logger
+diff -ru /mnt/sec/cache /home/sec/backup
