a2e490a401e448d23a9fff190c2e56c172457049
Hazeman99 <hazem.xbox@gmail.com>
2021-12-15 15:47:55 +0300

    transferring db_backup to ftp server

--- infra/root/db_backup.sh
+++ infra/root/db_backup.sh
@@ -1,7 +1,14 @@
+. ./.creds
+
 date=$(date +%Y%m%d)
 
 echo "Doing the backup in /home/sec/db_backup/db_$date" && pg_basebackup -D /home/sec/db_backup/db_$date -U sec && 
 echo "Change dir to /home/sec/db_backup/db_$date" && cd /home/sec/db_backup && 
 echo "Compressing to db_$date.tar.gz--doing" && tar -zcf db_$date.tar.gz--doing ./db_$date && 
 echo "Moving/Renaming compressed file to ../db_$date.tar.gz" && mv db_$date.tar.gz--doing db_$date.tar.gz &&
-echo "Deleting uncompressed files at /home/sec/db_backup/db_$date" && cd .. && rm -r /home/sec/db_backup/db_$date
+echo "Deleting uncompressed files at /home/sec/db_backup/db_$date" && cd .. && rm -r /home/sec/db_backup/db_$date &&
+lftp -c "set ftp:list-options -a;
+set ssl:check-hostname no;
+open ftp://$login:$password@$FTP_server; 
+lcd /home/sec/db_backup;
+mirror --reverse --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;" | logger
