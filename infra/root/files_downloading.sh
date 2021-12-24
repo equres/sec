223fce6cedea2a534df3c1e646ec3c706bacf151
Wojciech Adam Koszek <wojciech@koszek.com>
2021-12-24 09:34:52 -0800

    I'm proposing better design of this script

--- infra/root/files_downloading.sh
+++ infra/root/files_downloading.sh
@@ -1,1 +1,21 @@
-(/home/sec/sec dow index --verbose --syslog; /home/sec/sec dowz --verbose --syslog; /home/sec/sec index --verbose --syslog; /home/sec/sec indexz --verbose --syslog) && /home/sec/sec event --event cron --job files_downloading --status success || /home/sec/sec event --event cron --job files_downloading --status failed
+#!/bin/sh
+
+set -x
+set -e
+
+sec=/home/sec/sec
+
+$sec dow index --verbose --syslog
+status=failed
+if [ $# -eq 0 ]; then
+	status=success
+fi
+$sec event --event cron --job dow_index --status $status
+
+# let's rewrite the bottom to look like the top ^^^
+
+#$sec dowz --verbose --syslog
+#$sec index --verbose --syslog
+#$sec indexz --verbose --syslog 
+#
+#$sec event --event cron --job files_downloading --status failed
