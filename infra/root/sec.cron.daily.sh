6ed195fa956a888c26e26fa62a06aaa550c6d30a
Hazeman99 <hazem.xbox@gmail.com>
2021-12-28 00:33:04 +0300

    updated cron download script

---
+++ infra/root/sec.cron.daily.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -x
+set -e
+
+sec=/home/sec/sec
+
+$sec dow index --verbose --syslog
+status=failed
+if [ $# -eq 0 ]; then
+	status=success
+fi
+$sec event --event cron --job dow_index --status $status
+
+$sec dowz --verbose --syslog
+status=failed
+if [ $# -eq 0 ]; then
+	status=success
+fi
+$sec event --event cron --job dowz --status $status
+
+$sec index --verbose --syslog
+status=failed
+if [ $# -eq 0 ]; then
+	status=success
+fi
+$sec event --event cron --job index --status $status
+
+$sec indexz --verbose --syslog
+status=failed
+if [ $# -eq 0 ]; then
+	status=success
+fi
+$sec event --event cron --job indexz --status $status
