8d66d9f186ceed638cf1082a82d9b85e49eecc8f
Hazeman99 <hazem.xbox@gmail.com>
2022-02-02 00:11:37 +0300

    created backups user

--- infra/root/backup_restore.sh
+++ infra/root/backup_restore.sh
@@ -3,5 +3,5 @@
 lftp -c "set ftp:list-options -a;
 set ssl:check-hostname no;
 open ftp://$login:$password@$FTP_server; 
-lcd /home/sec/backup;
+lcd /home/backups/backup;
 mirror --delete --use-cache --verbose --allow-chown  --allow-suid --no-umask --parallel=2;" | logger
