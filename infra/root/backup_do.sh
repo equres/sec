4b4b239c7326a9dcf24a6507a422492cae8f2031
Hazeman99 <hazem.xbox@gmail.com>
2022-02-07 23:25:48 +0300

    combined backup files

--- infra/root/backup_do.sh
+++ infra/root/backup_do.sh
@@ -5,30 +5,24 @@ set -e
 
 sec=/home/sec/sec
 
-/root/backup_store.sh
+date=$(date +%Y%m%d)
+
+# Create Cache Backup
+tar -cJf cache_$date.tar.xz /mnt/sec/cache
 status=failed
 if [ $# -eq 0 ]; then
 	status=success
 fi
-$sec event --event cron --job backup_store --status $status
+$sec event --event cron --job cache_compressing --status $status
 
-/root/backup_restore.sh
+echo "Doing the backup in /home/backups/db_backup/db_$date" && pg_basebackup -D /home/backups/db_backup/db_$date && 
+echo "Change dir to /home/backups/db_backup/db_$date" && cd /home/backups/db_backup && 
+echo "Compressing to db_$date.tar.xz--doing" && tar cfJ db_$date--doing.tar.xz ./db_$date &&
+echo "Deleting uncompressed files at /home/backups/db_backup/db_$date" && cd .. && rm -r /home/backups/db_backup/db_$date &&
+echo "Moving/Renaming compressed file to ../db_$date.tar.xz" && mv db_$date--doing.tar.xz db_$date.tar.xz
 status=failed
-if [ $# -eq 0 ]; then
-	status=success
-fi
-$sec event --event cron --job backup_restore --status $status
-
-/root/backup_verify.sh
-status=failed
-if [ $# -eq 0 ]; then
-	status=success
-fi
-$sec event --event cron --job backup_verify --status $status
-
-/root/db_backup.sh
-status=failed
-if [ $# -eq 0 ]; then
+FILE=/home/backups/db_backup/db_$date.tar.xz
+if [ -f "$FILE" ]; then
 	status=success
 fi
 $sec event --event cron --job db_backup --status $status
