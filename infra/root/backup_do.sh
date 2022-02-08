28a4d997c5a3365f9f1f82891ce045401d791c85
Hazeman99 <hazem.xbox@gmail.com>
2022-02-08 23:37:36 +0300

    removed backups user sec config

--- infra/root/backup_do.sh
+++ infra/root/backup_do.sh
@@ -8,21 +8,23 @@ sec=/home/sec/sec
 date=$(date +%Y%m%d)
 
 # Create Cache Backup
-tar -cJf cache_$date.tar.xz /mnt/sec/cache
+tar -cJf cache_$date--doing.tar.xz /mnt/sec/cache
+mv cache_$date--doing.tar.xz cache_$date.tar.xz
 status=failed
 if [ $# -eq 0 ]; then
 	status=success
 fi
-$sec event --event cron --job cache_compressing --status $status
+$sec event --event cron --job cache_compressed --status $status --config /home/sec/.config/sec/config.yaml
 
-echo "Doing the backup in /home/backups/db_backup/db_$date" && pg_basebackup -D /home/backups/db_backup/db_$date && 
-echo "Change dir to /home/backups/db_backup/db_$date" && cd /home/backups/db_backup && 
-echo "Compressing to db_$date.tar.xz--doing" && tar cfJ db_$date--doing.tar.xz ./db_$date &&
-echo "Deleting uncompressed files at /home/backups/db_backup/db_$date" && cd .. && rm -r /home/backups/db_backup/db_$date &&
-echo "Moving/Renaming compressed file to ../db_$date.tar.xz" && mv db_$date--doing.tar.xz db_$date.tar.xz
+pg_basebackup -D /home/backups/db_backup/db_$date
+cd /home/backups/db_backup 
+tar cfJ db_$date--doing.tar.xz ./db_$date
+mv /home/backups/db_backup/db_$date--doing.tar.xz /home/backups/db_backup/db_$date.tar.xz
+cd .. && rm -r /home/backups/db_backup/db_$date
+
 status=failed
 FILE=/home/backups/db_backup/db_$date.tar.xz
 if [ -f "$FILE" ]; then
 	status=success
 fi
-$sec event --event cron --job db_backup --status $status
+$sec event --event cron --job db_backup --status $status --config /home/sec/.config/sec/config.yaml
